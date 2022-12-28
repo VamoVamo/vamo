a750f7acfc1b39ad611cec16c695b90ccd99314e
brianunlam <brian.alonso.a@gmail.com>
2022-12-28 14:36:17 -0300

    Add eslint and prettier to components

--- packages/components/src/components/Button/Button.test.tsx
+++ packages/components/src/components/Button/Button.test.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { render } from "@testing-library/react";
+import React from 'react';
+import { render } from '@testing-library/react';
 
-import Button from "./Button";
+import Button from './Button';
 
 console.log({Button});
 
-describe("Button", () => {
-  test("renders the Button component", () => {
-    render(<Button label="Hello world!" />);
-  });
+describe('Button', () => {
+	test('renders the Button component', () => {
+		render(<Button label="Hello world!" />);
+	});
 });
