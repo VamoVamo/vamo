c0bbe2eb883c94bde1ed859f6d3933af55d630f4
brianunlam <brian.alonso.a@gmail.com>
2022-12-27 23:20:32 -0300

    Add jests tests to components

---
+++ packages/components/src/components/Button/Button.test.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import Button from "./Button";
+
+console.log({Button});
+
+describe("Button", () => {
+  test("renders the Button component", () => {
+    render(<Button label="Hello world!" />);
+  });
+});
